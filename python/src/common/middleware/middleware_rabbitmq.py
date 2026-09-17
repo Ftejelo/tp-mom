71eff4e7ed8b8b49b80c701dfab42191ea7fdda5
ffer11 <ftejelo@fi.uba.ar>
2026-09-16 21:17:33 -0300

    Cambio approach (TDD), minimo código para que pase el test test_init_and_close():

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,87 +1,32 @@
 import pika
-from .middleware import (
-    MessageMiddlewareQueue, 
-    MessageMiddlewareExchange,
-    MessageMiddlewareDisconnectedError,
-    MessageMiddlewareMessageError,
-    MessageMiddlewareCloseError
-)
+import random
+import string
+from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        self._host = host
-        self._queue_name = queue_name
-        self._connection = None
-        self._channel = None
-        self._consuming = False
-        self._connect()
-
-    def _connect(self):
-        try:
-            self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self._host))
-            self._channel = self._connection.channel()
-            self._channel.queue_declare(queue=self._queue_name)
-        except Exception as e:
-            raise MessageMiddlewareDisconnectedError(f"Failed to connect to RabbitMQ: {e}")
-
-    def start_consuming(self, on_message_callback):
-        
-        def callback(ch, method, body):
-            try:
-                def ack():
-                    ch.basic_ack(delivery_tag=method.delivery_tag)
-                
-                def nack():
-                    ch.basic_nack(delivery_tag=method.delivery_tag)
-                
-                on_message_callback(body, ack, nack)
-            except Exception as e:
-                raise MessageMiddlewareMessageError(f"Error in message callback: {e}")
-
-        try:
-            self._channel.basic_consume(queue=self._queue_name, on_message_callback=callback)
-            self._consuming = True
-            self._channel.start_consuming()
-        except pika.exceptions.ConnectionClosed:
-            raise MessageMiddlewareDisconnectedError("Connection lost while consuming")
-        except Exception as e:
-            raise MessageMiddlewareMessageError(f"Error while consuming: {e}")
-
-    def stop_consuming(self):
-        if self._consuming and self._channel and not self._channel.is_closed:
-            try:
-                self._channel.stop_consuming()
-                self._consuming = False
-            except pika.exceptions.ConnectionClosed:
-                raise MessageMiddlewareDisconnectedError("Connection lost while stopping consumption")
-            except Exception as e:
-                raise MessageMiddlewareMessageError(f"Error while stopping consumption: {e}")
-
-    def send(self, message):
-        try:
-            self._channel.basic_publish(
-                exchange='',
-                routing_key=self._queue_name,
-                body=message
-            )
-        except pika.exceptions.ConnectionClosed:
-            raise MessageMiddlewareDisconnectedError("Connection lost while sending message")
-        except Exception as e:
-            raise MessageMiddlewareMessageError(f"Error while sending message: {e}")
+        self.host = host
+        self.queue_name = queue_name
 
     def close(self):
-        try:
-            if self._consuming:
-                self.stop_consuming()
-            if self._channel and not self._channel.is_closed:
-                self._channel.close()
-            if self._connection and not self._connection.is_closed:
-                self._connection.close()
-        except Exception as e:
-            raise MessageMiddlewareCloseError(f"Error while closing connection: {e}")
+        pass
+
+    def send(self, message):
+        pass
+
+    def start_consuming(self, on_message_callback):
+        pass
+
+    def stop_consuming(self):
+        pass
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
+        self.host = host
+        self.exchange_name = exchange_name
+        self.routing_keys = routing_keys
+
+    def close(self):
         pass
