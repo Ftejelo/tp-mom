c42d9aff483891d7c6cf0f498fdab79d24187c54
ffer11 <ftejelo@fi.uba.ar>
2026-09-16 21:41:14 -0300

    cambios para que pase el test test_direct_messaging_many_consumers_many_messages

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,7 +1,7 @@
 import pika
-import random
-import string
+
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
@@ -27,6 +27,58 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
         self.host = host
         self.exchange_name = exchange_name
         self.routing_keys = routing_keys
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+        self._channel = self._connection.channel()
+        self._channel.exchange_declare(exchange=self.exchange_name, exchange_type="direct")
+        self._consumer_queue = None
+        self._should_consume = True
 
     def close(self):
-        pass
+        if self._connection and self._connection.is_open:
+            self._connection.close()
+
+    def send(self, message):
+        for routing_key in self.routing_keys:
+            self._channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key=routing_key,
+                body=message,
+            )
+
+    def start_consuming(self, on_message_callback):
+        self._should_consume = True
+        result = self._channel.queue_declare(queue="", exclusive=True)
+        self._consumer_queue = result.method.queue
+
+        for routing_key in self.routing_keys:
+            self._channel.queue_bind(
+                exchange=self.exchange_name,
+                queue=self._consumer_queue,
+                routing_key=routing_key,
+            )
+
+        def _on_message(channel, method, body):
+            if not self._should_consume:
+                channel.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+                channel.stop_consuming()
+                return
+
+            def _ack():
+                channel.basic_ack(delivery_tag=method.delivery_tag)
+
+            def _nack():
+                channel.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+
+            on_message_callback(body, _ack, _nack)
+
+        self._channel.basic_consume(
+            queue=self._consumer_queue,
+            on_message_callback=_on_message,
+            auto_ack=False,
+        )
+        self._channel.start_consuming()
+
+    def stop_consuming(self):
+        self._should_consume = False
+        if self._channel and self._channel.is_open:
+            self._channel.stop_consuming()
